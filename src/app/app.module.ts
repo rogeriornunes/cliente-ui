6359c562ef9f79c84ac8a80c9020008cbd571ffc
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-11 00:09:09 -0300

    Commit inicial da estrutura do cliente-ui

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {ClienteComponent} from './cliente/cliente.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ClienteService} from "./service/cliente.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClienteComponent
+  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+  providers: [ClienteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
