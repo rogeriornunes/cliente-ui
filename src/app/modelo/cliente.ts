fab0750bdd136e731c7beb06b7bb5d6637e40193
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-11 23:11:19 -0300

    Implementação inicial do formulario de cadastro do cliente

---
+++ src/app/modelo/cliente.ts
@@ -0,0 +1,12 @@
+export interface Cliente {
+  id?: number;
+  nome?: string;
+  rg?: string;
+  cpf?: string;
+  cnpj?: string;
+  ie?: string;
+  telefonePrincipal?: string;
+  telefoneAlternativo?: string;
+  status?: boolean;
+  dataCadastro?: string;
+}
