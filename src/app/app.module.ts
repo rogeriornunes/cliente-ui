c9198debc8d148456c9c6dc24098b737537ed57c
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-11 23:09:58 -0300

    Adicionando lib para mask e configuração  environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import {ClienteComponent} from './cliente/cliente.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {ClienteService} from "./service/cliente.service";
+import {NgxMaskModule} from "ngx-mask";
 
 @NgModule({
   declarations: [
     AppComponent,
     ClienteComponent
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot({validation: true})
+  ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
 })
