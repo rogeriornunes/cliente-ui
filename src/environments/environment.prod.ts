b97b2d84592074fdf2cdc4371f21039175051409
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-15 18:50:47 -0300

    Apontamento de servidor do heroku para cliente-api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://cliente-api:8080'
+  //apiBaseUrl: 'http://cliente-api:8080'
+  apiBaseUrl: 'https://cliente-api.herokuapp.com'
 };
