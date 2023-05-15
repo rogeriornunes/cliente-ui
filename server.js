6225c3634d9bc6e1bf3eff10776b8591b648410b
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-15 18:33:40 -0300

    Ajuste de build heroku

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// importar express
 const express = require('express');
-
+// iniciar express
 const app = express();
+// nome da pasta no dist que sera feito o build
+const appName = 'cliente-ui';
+// local onde build ira gerar os arquivos
+const outputPath = `${__dirname}/dist/${appName}`;
 
-const PORT = process.env.PORT || 4200;
-
-app.use(express.static(__dirname + '/dist'));
-
+// seta o diretorio de build para servir o conteudo Angular
+app.use(express.static(outputPath));
+// redirecionar qualquer requisicao para o index.html
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(`${outputPath}/index.html`);
 });
-
-app.listen(PORT, () => {
-    console.log('Servidor iniciado na porta: ' + PORT);
-})
+// ouvir a porta que o Heroku disponibilizar
+app.listen(process.env.PORT);
